d4077cf4e40e6842711babefcb148b7e2c0b67c9
Anthony Cook <ddragonmaster49@yahoo.com>
2016-05-04 16:05:02 +0000

    Altered file path so it can be correctly resolved regardless of where it is executed

--- db/country_name_retriever.rb
+++ db/country_name_retriever.rb
@@ -1,7 +1,7 @@
 class CountryNameRetriever
     
     def retrieve_names 
-        File.open("#{Dir.pwd}/../country_info/lat_long.txt", "r") do |f|
+        File.open("#{File.dirname(__FILE__)}/../country_info/lat_long.txt", "r") do |f|
            lines = f.readlines("\n")
            names = lines.map do |line|
                line.split(",")[0]
