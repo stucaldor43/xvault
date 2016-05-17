de23b312e36e3a853897c7923a6d657de803d24d
Anthony Cook <ddragonmaster49@yahoo.com>
2016-05-17 18:15:12 +0000

    Allow database building script to add comments to database

--- db/prepare_db.rb
+++ db/prepare_db.rb
@@ -6,6 +6,7 @@ require_relative "image_url_retriever"
 require_relative "database_manager"
 require_relative "../lib/s3_bucket_manager"
 require "dragonfly"
+require "lorem"
 
 # implement database schema
 applier = SchemaApplier.new
@@ -31,8 +32,8 @@ end
 s3_manager = S3BucketManager.new
 s3_image_hash_list = [] 
 dragonfly_content_hash_collection.each do |hash|
-    s3_original_image_url = s3_manager.insert_image(hash[:larger_image])
-    s3_thumb_image_url = (s3_original_image_url) ?  s3_manager.insert_image(hash[:smaller_image]) : nil
+    s3_original_image_url = s3_manager.insert_file(hash[:larger_image])
+    s3_thumb_image_url = (s3_original_image_url) ?  s3_manager.insert_file(hash[:smaller_image]) : nil
     if (s3_original_image_url && s3_thumb_image_url)
         s3_image_hash_list << {original: s3_original_image_url, thumb: s3_thumb_image_url}
     end
@@ -41,4 +42,17 @@ end
 pg_manager = DatabaseManager.new
 s3_image_hash_list.each do |img_pair|
     pg_manager.insert_images(img_pair)
+end
+
+picture_primary_keys_list = pg_manager.execute_statement(
+"SELECT picture_id FROM picture").column_values(0).map(&:to_i)
+
+picture_primary_keys_list.each do |picture_record_primary_key|
+    comment_count = rand(0..5)
+    if comment_count > 0
+      comment_count.times do |i|
+        message = Lorem::Base.new(Lorem::Base::TYPES[1], rand(50..100)).output
+        pg_manager.insert_comment(message, picture_record_primary_key)
+      end
+    end
 end
